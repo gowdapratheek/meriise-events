226063c516e32c9beb7b2e55d7f7707e978f2d49
H G Pratheek Gowda <93853432+gowdapratheek@users.noreply.github.com>
2024-05-14 13:08:01 +0530

    total event wip

---
+++ src/pages/Event21.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "/src/components/Header";
+import Carousel from "/src/components/Carousel";
+
+function Event22() {
+  return (
+    <>
+      <Header />
+      <div className="lg:grid lg:grid-cols-2 lg:gap-4">
+        {/* <Carousel images={["", "", ""]} title="" description="" date="" /> */}
+
+        <Carousel
+          images={[
+            "/assets/events23/idea2.jpg",
+            "/assets/events23/idea3.jpg",
+            "/assets/events23/idea5.jpg",
+            "/assets/events23/idea4.jpg",
+            "/assets/events23/idea1.jpg",
+          ]}
+          title="From Ideas to Safe Guarding your Ideas"
+          description="The event “From Ideas to Safe Guarding your Ideas” served as a catalyst, igniting knowledge on Intellectual property rights in innovation. The event focussed on fostering the students with the various stages of Innovation. 
+"
+          date="2nd November, 2023"
+        />
+      </div>
+    </>
+  );
+}
+
+export default Event22;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Header from "/src/components/Header";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <div className="mt-[20vh]">
+        <div className="[&_*]: text-[#01adfc] [&_*]:bg-black [&_*]:w-[40vh] [&_*]:h-[10vh] [&_*]:rounded-2xl flex flex-col justify-center items-center    lg:grid lg:grid-cols-2 lg:gap-4 lg:[&_*]:place-self-center">
+         
+          {/* <div className="my-5">
+            <p>
+              <Link
+                to="/"
+                className="text-3xl flex justify-center items-center"
+              >
+                2023
+              </Link>
+            </p>
+          </div> */}
+
+          <div className="my-5">
+            <p>
+              <Link
+                to="/events23"
+                className="text-3xl flex justify-center items-center"
+              >
+                2023
+              </Link>
+            </p>
+          </div>
+
+          <div className="my-5">
+            <p>
+              <Link
+                to="/events22"
+                className="text-3xl flex justify-center items-center"
+              >
+                2022
+              </Link>
+            </p>
+          </div>
+          
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;

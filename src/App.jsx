07218741e997de752b3eb993b4f6c2c52bb26916
Shravani G <shravanig18@gmail.com>
2024-06-15 07:56:58 +0530

    events 24 done
    good morning!!! senpai

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Event24 from "./pages/Event24";
 import Event23 from "./pages/Event23";
 import Event22 from "./pages/Event22";
 import Event21 from "./pages/Event21";
@@ -7,12 +8,12 @@ import Event20 from "./pages/Event20";
 import Event19 from "./pages/Event19";
 import Event18 from "./pages/Event18";
 
-
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       {/* <Route exact path="/" element={<Event23 />} /> */}
+      <Route exact path="/events24" element={<Event24 />} />
       <Route exact path="/events23" element={<Event23 />} />
       <Route exact path="/events22" element={<Event22 />} />
       <Route exact path="/events21" element={<Event21 />} />

7960f16bdb9c24517de8f0a94be9d51db25b401f
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2024-09-03 15:48:04 +0530

    Updated with jpeg and png error resolved
    Updated Innovation Expedition and Soft skills Training Programme images..

--- src/pages/Event24.jsx
+++ src/pages/Event24.jsx
@@ -8,21 +8,21 @@ function Event24() {
                 {/* <Carousel images={["", "", ""]} title="" description="" date="" /> */}
                 <Carousel
                     images={[
-                        "/assets/events24/Pic1.jpg",
-                        "/assets/events24/Pic2.jpg",
-                        "/assets/events24/Pic3.jpg",
-                        "/assets/events24/Pic4.jpg",
-                        "/assets/events24/Pic5.jpg",
+                        "/assets/events24/Pic1.png",
+                        "/assets/events24/Pic2.jpeg",
+                        "/assets/events24/Pic3.jpeg",
+                        "/assets/events24/Pic4.jpeg",
+                        "/assets/events24/Pic5.jpeg",
                     ]}
-                    title="Innovation Expedition - Innovation and Entrepreneurship Exposure"
+                    title="Innovation Expedition Entrepreneurship Exposure"
                     description="Two day - Innovation Expedition - Innovation and Entrepreneurship Exposure Visit for Mentee Institutes on 19th and 20th July,2024"
                     date="19th and 20th July, 2024"
                 />
                 <Carousel
                     images={[
-                        "/assets/events24/P1.jpg",
-                        "/assets/events24/P2.jpg",
-                        "/assets/events24/p3.jpg",
+                        "/assets/events24/P1.jpeg",
+                        "/assets/events24/P2.jpeg",
+                        "/assets/events24/P3.jpeg",
                     ]}
                     title="Soft Skills Training Programme"
                     description="A three-day soft skills training program was organized from 08-07-2024 to 10-07-2024 for various programmes. Mr. Santhosh, facilitated the training on all the three days. The training programme aimed to enhance participants' interpersonal skills, communication abilities, and overall professional effectiveness.Participants gained proficiency in Adobe Photoshop and Illustrator, allowing them to create graphic designs, and design user interfaces for websites and applications. Additionally, they learnt advanced techniques in graphic design and video editing using Adobe After Effects, enhancing their ability to create visually appealing content across different platforms."

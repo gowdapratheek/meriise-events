9e0d4cfdec3ce24c9dfd91ba4a6c2acb41c32efb
Darshan-Babu-19 <techassistmce@gmail.com>
2025-03-12 17:06:10 +0530

    VC visit added

--- src/pages/Event24.jsx
+++ src/pages/Event24.jsx
@@ -8,6 +8,18 @@ function Event24() {
                 {/* <Carousel images={["", "", ""]} title="" description="" date="" /> */}
                 <Carousel
                     images={[
+                        "/assets/events24/VC1.jpeg",
+                        "/assets/events24/VC2.jpeg",
+                        "/assets/events24/VC3.jpeg",
+                        "/assets/events24/VC4.jpeg",
+                        "/assets/events24/VC5.jpeg",
+                    ]}
+                    title="Collaboration meeting with the Vice-Chancellors of Kodagu and Hassan University on 12th March, 2025 at Malnad College of Engineering, Hassan"
+                    description="The meeting aimed to explore potential areas of collaboration between Malnad College of Engineering (MCE), ME-RIISE Foundation, and Kodagu & Hassan University. The key focus was on initiatives promoting entrepreneurship, skill enhancement, and innovation-led development within the region."
+                    date="12th March,2025"
+                />
+                <Carousel
+                    images={[
                         "/assets/events24/VP1.JPG",
                         "/assets/events24/VP2.jpeg",
                     ]}
